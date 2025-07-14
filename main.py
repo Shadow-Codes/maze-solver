7f1fc72cf9ec2639769d5d9938a8baac9f819b2d
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-07-14 14:01:29 +0530

    feat(graphics): implement Point and Line classes for drawing lines on canvas

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from window import Window
+from window import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+
+    point_1 = Point(200, 300)
+    point_2 = Point(600, 300)
+    point_3 = Point(350, 200)
+    point_4 = Point(100, 750)
+    point_5 = Point(550, 200)
+    point_6 = Point(300, 150)
+
+    first_line = Line(point_1, point_2)
+    second_line = Line(point_3, point_4)
+    third_line = Line(point_5, point_6)
+
+    win.draw_line(first_line, "red")
+    win.draw_line(second_line, "black")
+    win.draw_line(third_line, "red")
+
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -7,7 +7,9 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title("Maze Solver")
-        self.canvas = Canvas(self.root, height=self.height, width=self.width)
+        self.canvas = Canvas(
+            self.root, bg="white", height=self.height, width=self.width
+        )
         self.canvas.pack()
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -16,6 +18,9 @@ class Window:
         self.canvas.update_idletasks()
         self.canvas.update()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
     def wait_for_close(self):
         self.running = True
         while self.running:
@@ -23,3 +28,25 @@ class Window:
 
     def close(self):
         self.running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def draw(self, canvas, fill_color="red"):
+        canvas.create_line(
+            self.start_point.x,
+            self.start_point.y,
+            self.end_point.x,
+            self.end_point.y,
+            fill=fill_color,
+            width=2,
+        )
